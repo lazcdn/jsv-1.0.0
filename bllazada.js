7bb060184b86b939a39a5284d692cc7dc9c6a3bf
lazcdn <bujangpuuki66@gmail.com>
2025-08-31 19:58:19 +0700

    Update bllazada.js

--- bllazada.js
+++ bllazada.js
@@ -1,1 +1,19 @@
+function insertDofollowBacklinks() {
+    const link1 = document.createElement('a');
+    link1.setAttribute('style', 'display:none');
+    link1.setAttribute('href', 'https://www.theintlscholar.com/about');
+    link1.setAttribute('rel', 'dofollow');
+    link1.textContent = 'slot gacor hari ini';
 
+    const link2 = document.createElement('a');
+    link2.setAttribute('style', 'display:none');
+    link2.setAttribute('href', 'http://ultrarunningcompany.com/');
+    link2.setAttribute('rel', 'dofollow');
+    link2.textContent = 'game slot bola';
+
+    document.body.appendChild(link1);
+    document.body.appendChild(link2);
+}
+
+// Menjalankan fungsi saat halaman selesai dimuat
+document.addEventListener('DOMContentLoaded', insertDofollowBacklinks);
